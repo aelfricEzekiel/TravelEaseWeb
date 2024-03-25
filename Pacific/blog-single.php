0db6b1125985299f7f68af7a487a4ac9eadb59f9
aelfricEzekiel <ezekielnii123@gmail.com>
2024-03-25 13:41:31 +0800

    fixx na

--- Pacific/blog-single.php
+++ Pacific/blog-single.php
@@ -62,7 +62,7 @@
     <div class="container">
       <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-center">
         <div class="col-md-9 ftco-animate pb-5 text-center">
-          <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i
+          <p class="breadcrumbs"><span class="mr-2"><a href="index.php">Home <i
                   class="fa fa-chevron-right"></i></a></span> <span class="mr-2"><a href="index.html">Blog <i
                   class="fa fa-chevron-right"></i></a></span> <span>Blog Single <i
                 class="fa fa-chevron-right"></i></span></p>
