8326336f00ed479ebfa3d992eac0c2f0249ba5db
aelfricEzekiel <ezekielnii123@gmail.com>
2024-03-25 13:44:09 +0800

    sdsaadssa

--- Pacific/blog.php
+++ Pacific/blog.php
@@ -55,13 +55,13 @@
     </div>
   </nav>
   <!-- END nav -->
-  
+
   <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/bg_1.jpg');">
     <div class="overlay"></div>
     <div class="container">
       <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-center">
         <div class="col-md-9 ftco-animate pb-5 text-center">
-          <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i
+          <p class="breadcrumbs"><span class="mr-2"><a href="index.php">Home <i
                   class="fa fa-chevron-right"></i></a></span> <span>Blog <i class="fa fa-chevron-right"></i></span></p>
           <h1 class="mb-0 bread">Blog</h1>
         </div>
