8305bc4a44ba615f5740105cc88b0c50640b76d8
aelfricEzekiel <ezekielnii123@gmail.com>
2024-03-30 16:05:30 +0800

    naisip nagid mn ang tourist accounts kung pila kabilog

--- Pacific/Admin Dashboard/tourBooking.php
+++ Pacific/Admin Dashboard/tourBooking.php
@@ -271,13 +271,6 @@
         </div>
     </div>
     
-    <!-- loader -->
-	<div id="ftco-loader" class="show fullscreen">
-		<svg class="circular" width="48px" height="48px">
-			<circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
-			<circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" />
-		</svg>
-	</div>
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
